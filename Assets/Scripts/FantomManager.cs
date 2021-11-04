dc053c40f84f3db7e4bbb805df13c22c1bcc5d0d
nzeyvan <nzeyvan@hotmail.fr>
2021-11-04 15:40:14 +0100

    fantom controller

---
+++ Assets/Scripts/FantomManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FantomManager : MonoBehaviour
+{
+
+    [SerializeField] private float interval;
+    [SerializeField] private Object spawnedObject;
+    [SerializeField] private int max;
+
+    private int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Create();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator CreateFantom()
+    {
+        while (count < max)
+        {
+            yield return new WaitForSeconds(interval);
+            Instantiate(spawnedObject, new Vector3(Random.Range(-5f, 5f), 6), transform.rotation);
+            count++;
+        }
+    }
+
+    private void Create()
+    {
+        StartCoroutine(CreateFantom());
+    }
+}
+
