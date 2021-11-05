b55836eaf99765f501e2bd7c74b4d34c488acc91
Joachim Bastin Local <joachim.bastin@gmail.com>
2021-11-05 18:46:17 +0100

    reefactor gameController & friends

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,36 +3,80 @@ using UnityEngine;
 
 public class GameManager : SingletonBehaviour<GameManager>
 {
-    [SerializeField] private string startScene;
-    [SerializeField] GameObject panelPause;
+    [SerializeField] GameObject mainMenuPanel;
+    [SerializeField] GameObject diePanel;
+    [SerializeField] GameObject pausePanel;
+	private const string KEY_CURRENT_LEVEL = "currentLevel";
+	private int progress => PlayerPrefs.GetInt(KEY_CURRENT_LEVEL);
+	private bool isMenuOpen = false;
 
-    public void Quit()
+	private void Start()
+	{
+		if (progress == 0)
+			PlayerPrefs.SetInt(KEY_CURRENT_LEVEL, 1); // on initialise le progrès au niveau 1
+	}
+
+	// escape to pause
+	private void Update()
+	{
+		if (Input.GetButtonDown("Cancel") && SceneManager.GetActiveScene().buildIndex != 0) // on fait pause hors du menu principal
+		{
+			isMenuOpen = !isMenuOpen;
+			pausePanel.SetActive(isMenuOpen);
+			if (isMenuOpen)
+				Time.timeScale = 0;
+			else
+				Time.timeScale = 1;
+		}
+	}
+
+	// MENU BUTTON
+	public void Continue()
+	{
+		isMenuOpen = false;
+		pausePanel.SetActive(isMenuOpen);
+		Time.timeScale = 1;
+	}
+
+	public void RestartLevel()
+	{
+		Continue();
+		SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void Menu()
+	{
+		Continue();
+		mainMenuPanel.SetActive(true);
+		SceneManager.LoadSceneAsync(0);
+	}
+
+	public void Exit()
     {
         Debug.Log("Je quitte le jeu");
         Application.Quit();
     }
 
-    public void StartGame()
-    {
-        Time.timeScale = 1;
-        SceneManager.LoadScene(1);
-    }
+	public void ContinueProgress()
+	{
+		mainMenuPanel.SetActive(false);
+		int progress = PlayerPrefs.GetInt(KEY_CURRENT_LEVEL);
+		SceneManager.LoadSceneAsync(progress);
+	}
 
-    public void Continue()
-    {
-        panelPause.SetActive(false);
-        Time.timeScale = 1;
-    }
 
-    public void Menu()
-    {
-        SceneManager.LoadScene(0);
-    }
+	// PORATL METHOD
+	public void PreviousLevel()
+	{
+		SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex - 1);
+	}
 
-    public void Restart()
-    {
-        Time.timeScale = 1;
-        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
-    }
+	public void NextLevel()
+	{
+		int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+		PlayerPrefs.SetInt(KEY_CURRENT_LEVEL, nextScene);
+		PlayerPrefs.Save();
+		SceneManager.LoadSceneAsync(nextScene);
+	}
 
 }
