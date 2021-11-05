d65a8413a40d2b7cd4825da8d2142177b3589903
Joachim Bastin Local <joachim.bastin@gmail.com>
2021-11-05 16:32:06 +0100

    add next and previous level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,10 @@ public class GameManager : SingletonBehaviour<GameManager>
 {
     [SerializeField] private string startScene;
     [SerializeField] GameObject panelPause;
+	private const string KEY_CURRENT_LEVEL = "currentLevel";
 
-    public void Quit()
+
+	public void Quit()
     {
         Debug.Log("Je quitte le jeu");
         Application.Quit();
@@ -34,5 +36,24 @@ public class GameManager : SingletonBehaviour<GameManager>
         Time.timeScale = 1;
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
     }
+
+	public void ContinueProgress()
+	{
+		int progress = PlayerPrefs.GetInt(KEY_CURRENT_LEVEL);
+		SceneManager.LoadSceneAsync(progress);
+	}
+
+	public void PreviousLevel()
+	{
+		SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex - 1);
+	}
+
+	public void NextLevel()
+	{
+		int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+		PlayerPrefs.SetInt(KEY_CURRENT_LEVEL, nextScene);
+		PlayerPrefs.Save();
+		SceneManager.LoadSceneAsync(nextScene);
+	}
 
 }
