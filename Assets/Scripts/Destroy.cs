ebca9b7e9b59c813ec42994db4616a9b79791745
Senhaji Obey <obey.senhaji@student.vinci.be>
2021-11-05 17:33:03 +0100

    fix commit

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Destroy : MonoBehaviour
+{
+    [SerializeField] private UnityEvent OnDeath;
+
+    // Start is called before the first frame update
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        OnDeath?.Invoke();
+        Destroy(collision.gameObject);
+    }
+}
