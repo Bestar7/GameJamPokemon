78abc14b8f60c335d5c19a68fab7188c039738fd
nzeyvan <nzeyvan@hotmail.fr>
2021-11-05 12:33:56 +0100

    quelque chose

--- Assets/Scripts/FantomManager.cs
+++ Assets/Scripts/FantomManager.cs
@@ -1,8 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterManager : MonoBehaviour
+public class FantomManager : MonoBehaviour
 {
 
     [SerializeField] private float interval;
@@ -11,7 +10,6 @@ public class CharacterManager : MonoBehaviour
     [SerializeField] private float distance;
 
     private int count = 0;
-    private LinkedList<Vector3> spawnPoints;
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/HitDetection.cs
+++ Assets/Scripts/HitDetection.cs
@@ -1,37 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using Random = UnityEngine.Random;
 
 
 public class HitDetection: MonoBehaviour
 {
+    [SerializeField] private LayerMask phantomLayer;
     [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private UnityEvent OnDeath;
 
-    public void OnTriggerStay2D(Collider2D collision)
-    {
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (playerLayer != (playerLayer | 1 << other.gameObject.layer)) return;
-        Destroy(other.gameObject);
-    }
-
-    /*
-    private void Relocate()
-    {
-        var offset = new Vector3(Random.Range(-10f, 10f), Random.RandomRange(-5f,5f));
-
-        Collider2D player = Physics2D.OverlapCircle(transform.position + offset, 4f, playerLayer);
-
-        if (player)
+        if (phantomLayer != (phantomLayer | 1 << other.gameObject.layer)) return;
+        if (other.IsTouchingLayers(playerLayer))
         {
-            Vector3 distance = player.attachedRigidbody.position;
-            distance = distance - transform.position;
-            var offset = 4f - (distance - Vector2.);
-
+            OnDeath?.Invoke();
+            Destroy(transform.gameObject);
         }
-            
     }
-    */
 }
 
